9922194dbcd0a6741a7b6a40c099aada48c0042a
Tony <ajb9271@psu.edu>
2025-08-14 02:59:42 -0500

    AI vs AI is working Human vs AI also working Before any cleanup Analytics working.

--- analytics_tracker.py
+++ analytics_tracker.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class AnalyticsTrackerSummary:
     def __init__(self, filename="Analytics\\game_stats_summary.csv"):
         self.filename = filename
-        self.fieldnames = ["timestamp", "play_id", "game_mode", "winner","win_moves", "win_shape", "lose",
+        self.fieldnames = ["play_id", "game_mode", "winner","win_moves", "win_shape", "lose",
                            "lose_shape", "lose_moves", "tiles_removed"]
         self._ensure_file_exists()
 
@@ -18,7 +18,6 @@ class AnalyticsTrackerSummary:
     def log_game(self, play_id, game_mode, winner, win_moves, win_shape,
                  lose, lose_moves, lose_shape, tiles_removed):
         log_entry = {
-            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
             "play_id": play_id,
             "game_mode": game_mode,
             "winner": winner,
@@ -36,7 +35,7 @@ class AnalyticsTrackerSummary:
 class AnalyticsTrackerDetails:
     def __init__(self, filename="Analytics\\game_stats_details.csv"):
         self.filename = filename
-        self.fieldnames = ["timestamp", "play_id", "game_mode","player","position",
+        self.fieldnames = ["play_id", "game_mode","player","position",
                            "shape","tiles_removed_pos"]
         self._ensure_file_exists()
 
@@ -48,13 +47,74 @@ class AnalyticsTrackerDetails:
 
     def log_game(self, play_id, game_mode, player,position, shape, tiles_removed_pos):
         log_entry = {
-            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
             "play_id": play_id,
             "game_mode": game_mode,
             "player": player,
             "position": position,
             "shape": shape,
             "tiles_removed_pos": tiles_removed_pos
+        }
+        with open(self.filename, mode="a", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+            writer.writerow(log_entry)
+class AnalyticsTrackerAgents:
+    def __init__(self, filename="Analytics\\game_stats_agents.csv"):
+        self.filename = filename
+        self.fieldnames = ["play_id", "game_mode", "AI1_type", "AI1_iterations", "AI2_type", "AI2_iterations"]
+        self._ensure_file_exists()
+
+    def _ensure_file_exists(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, mode="w", newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+                writer.writeheader()
+
+    def log_agents(self, play_id, game_mode, ai1_type, ai1_iterations, ai2_type, ai2_iterations):
+        log_entry = {
+            "play_id": play_id,
+            "game_mode": game_mode,
+            "AI1_type": ai1_type,
+            "AI1_iterations": ai1_iterations,
+            "AI2_type": ai2_type,
+            "AI2_iterations": ai2_iterations
+        }
+        with open(self.filename, mode="a", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+            writer.writerow(log_entry)
+
+class AnalyticsTrackerCombined:
+    def __init__(self, filename="Analytics\\game_stats_combined.csv"):
+        self.filename = filename
+        self.fieldnames = [
+            "play_id", "game_mode", "winner", "win_moves", "win_shape",
+            "lose", "lose_moves", "lose_shape", "tiles_removed",
+            "AI1_type", "AI1_iterations", "AI2_type", "AI2_iterations"
+        ]
+        self._ensure_file_exists()
+
+    def _ensure_file_exists(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, mode="w", newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+                writer.writeheader()
+
+    def log_combined(self, play_id, game_mode, winner, win_moves, win_shape,
+                     lose, lose_moves, lose_shape, tiles_removed,
+                     ai1_type, ai1_iterations, ai2_type, ai2_iterations):
+        log_entry = {
+            "play_id": play_id,
+            "game_mode": game_mode,
+            "winner": winner,
+            "win_moves": win_moves,
+            "win_shape": win_shape,
+            "lose": lose,
+            "lose_moves": lose_moves,
+            "lose_shape": lose_shape,
+            "tiles_removed": tiles_removed,
+            "AI1_type": ai1_type,
+            "AI1_iterations": ai1_iterations,
+            "AI2_type": ai2_type,
+            "AI2_iterations": ai2_iterations
         }
         with open(self.filename, mode="a", newline='') as file:
             writer = csv.DictWriter(file, fieldnames=self.fieldnames)
