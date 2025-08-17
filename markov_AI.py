42f44fabc0149bde5ce26da8884d2c90868158c9
Tony <ajb9271@psu.edu>
2025-08-17 13:10:19 -0500

    Final for submission

--- markov_AI.py
+++ markov_AI.py
@@ -1,4 +1,5 @@
 import csv
+import random
 from utils import check_winner, is_center
 
 class MarkovAgent:
@@ -21,27 +22,42 @@ class MarkovAgent:
 
         score = self.rewards.get("neutral", 0)
 
-        # Simulate move
+        ###--- Simulate AI move
         board_copy = [r[:] for r in board]
         board_copy[row][col] = self.symbol
 
+        ###--- Check if this move wins the game
         if check_winner(self.symbol, board_copy):
             score += self.rewards.get("win", 0)
-        elif check_winner(self.opponent, board_copy):
+
+        ###--- Simulate opponent move to detect threat
+        board_threat = [r[:] for r in board]
+        board_threat[row][col] = self.opponent
+        if check_winner(self.opponent, board_threat):
             score += self.rewards.get("block", 0)
 
+        ###--- Positional heuristic
         if is_center(board, row, col):
             score += self.rewards.get("center_control", 0)
 
         return score
-
     def select_move(self, board):
         best_score = float('-inf')
-        best_move = None
+        best_moves = []
         for r in range(len(board)):
             for c in range(len(board[r])):
                 score = self.evaluate_tile(board, r, c)
                 if score > best_score:
                     best_score = score
-                    best_move = (r, c)
-        return best_move
+                    best_moves = [(r, c)]
+                elif score == best_score:
+                    best_moves.append((r, c))
+        return random.choice(best_moves) if best_moves else None
+
+    def is_threat(board, r, c, opponent_symbol):
+        board[r][c] = opponent_symbol
+        threat = check_winner(board, opponent_symbol)
+        board[r][c] = None  # undo move
+        return threat
+
+
